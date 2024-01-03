1a9f9445864c38406b22bd1c3d740abd203b5fbe
SenKanna43 <senthamaraikannanp@gmail.com>
2024-01-03 18:26:18 +0530

    cutom hook

--- swiggy-clone/src/components/RestaurantMenu.js
+++ swiggy-clone/src/components/RestaurantMenu.js
@@ -1,30 +1,21 @@
-import { useEffect, useState } from "react";
-import { MENU_API } from "../utils/constant";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
-
   const { resId } = useParams();
 
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    setResInfo(json.data);
-  };
+  const resInfo = useRestaurantMenu(resId);
 
   if (resInfo === null) return <Shimmer />;
 
   const { name, cuisines, costForTwoMessage } =
     resInfo?.cards[0]?.card?.card?.info;
 
-  const { itemCards } =
+  const { itemCards, categories } =
     resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card;
+
+  const displayItems = itemCards || categories[0].itemCards;
 
   return (
     <div className="menu">
@@ -34,7 +25,7 @@ const RestaurantMenu = () => {
       </p>
       <h2>Menu</h2>
       <ul>
-        {itemCards.map((item) => (
+        {displayItems.map((item) => (
           <li key={item.card.info.id}>
             {item.card.info.name} -{" Rs."}
             {item.card.info.price / 100 || item.card.info.defaultPrice / 100}
